a4bf6de1cd70bba0c97796848e5bb3ee6317af3f
unknown <pkasu06@gmail.com>
2024-04-29 14:46:27 -0500

    added password genrator , updated readme file

---
+++ 05pwdGenerator/src/App.jsx
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllowed, charAllowed, setPassword])
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    numAllowed ? str += "0123456789" : null;
+    charAllowed ? str += "`!@#$%^&*(){}++_~<>?" : null;
+    for(let i= 0; i < length; i++){
+      pass += str.charAt(Math.ceil(Math.random() * str.length))
+    }
+
+    setPassword(pass);
+
+  }, [length, numAllowed, charAllowed, setPassword])
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg p-4 my-8 text-white bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input type="text" name="password" className="outline-none w-full py-1 px-3 text-orange-500" placeholder="password" value={password} readOnly />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range" min = {8} max = {100} value={length} className="cursor-pointer" onChange={(e) => setLength(e.target.value)}/>
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox" id="numInput" defaultChecked={numAllowed} onChange={() => setNumAllowed((prev) => !prev)} />
+            <label>numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox" id="charInput" defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)} />
+            <label>characters</label>
+          </div>
+        </div>
+      </div>
+    </>    
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # learn-react
 Read me file
+
+# To create a new project using react
+
+- `npm create vite@latest`
+
+# how to add tailwind in Vite project
+
+- 1) # Navigate to your Project directory: Make sure you are in the coorect project folder.
+- 2) # Installing tailwind and other dependencies: 
+    `npm install -D tailwindcss postcss autoprefixer`
+- 3) # Generate Configuration Files ( tailwind.config.js, postcss.config.js ) : 
+    `npx tailwindcss init -p`
+- 4) # Configure source/template paths :
+    - In `tailwind.config.cjs` file add this 
+        ```
+        content: [
+            "./index.html",
+            "./src/**/*.{html,js, jsx, ts, tsx}"
+        ],
+        ```
+- 5) # Adding Tailwind CSS Directives to your CSS file
+    - Add the following Tailwind CSS directives into file index.css or App.css or your most parent css file:
+    ```
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+    ```
