2f489d85ae5913ee7eba027b697f16f9a3a3e3ce
pkasu06 <pkasu06@gmail.com>
2024-05-03 19:38:56 -0500

    changes to recat router 07

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+function Github() {
+  useEffect
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers:</div>
+  )
+}
+
+export default Github
